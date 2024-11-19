d7ea48718ef4876433b007446ddfbdc1813fa24b
Gaurav Passi <er.gauravpassi@gmail.com>
2024-11-20 01:31:57 +0530

    fix: img masks naming

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 from torch.utils.data import Dataset
 import os
 import cv2
-    
+import numpy as np 
 
 class SlumSegmentationDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transform=None):
@@ -9,13 +9,22 @@ class SlumSegmentationDataset(Dataset):
         self.mask_dir = mask_dir
         self.transform = transform
         self.images = sorted(os.listdir(image_dir))
+        self.masks = sorted(os.listdir(mask_dir))  # Ensure masks are sorted consistently
+        # Optionally, build a mapping if necessary
+        self.image_to_mask = {img: f"mask_{img.split('_')[1]}" for img in self.images}
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.images[idx])
-        mask_path = os.path.join(self.mask_dir, self.images[idx].replace('.jpg', '_mask.png'))
+        img_name = self.images[idx]
+        mask_name = self.image_to_mask.get(img_name, None)
+        if mask_name is None:
+            raise ValueError(f"No corresponding mask found for image: {img_name}")
+
+        img_path = os.path.join(self.image_dir, img_name)
+        mask_path = os.path.join(self.mask_dir, mask_name)
+
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
