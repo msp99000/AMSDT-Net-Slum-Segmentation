837f6f228f19f7b7b565f3615525d2a236da5acd
Manpreet Singh <msp99000@gmail.com>
2024-11-27 01:29:02 +0530

    refactor: returns patch size instead of img size

--- dataset.py
+++ dataset.py
@@ -3,83 +3,6 @@ import os
 import cv2
 import numpy as np 
 
-# class SlumSegmentationDataset(Dataset):
-#     def __init__(self, image_dir, mask_dir, transform=None):
-#         self.image_dir = image_dir
-#         self.mask_dir = mask_dir
-#         self.transform = transform
-#         self.images = sorted(os.listdir(image_dir))
-#         self.masks = sorted(os.listdir(mask_dir))  # Ensure masks are sorted consistently
-#         self.image_to_mask = {img: f"mask_{img.split('_')[1]}" for img in self.images}
-
-#     def __len__(self):
-#         return len(self.images)
-
-#     def __getitem__(self, idx):
-#         img_name = self.images[idx]
-#         mask_name = self.image_to_mask.get(img_name, None)
-#         if mask_name is None:
-#             raise ValueError(f"No corresponding mask found for image: {img_name}")
-
-#         img_path = os.path.join(self.image_dir, img_name)
-#         mask_path = os.path.join(self.mask_dir, mask_name)
-
-#         image = cv2.imread(img_path)
-#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-
-#         if self.transform:
-#             augmented = self.transform(image=image, mask=mask)
-#             image = augmented['image']
-#             mask = augmented['mask']
-
-#         return image, mask
-
-
-# class SlumSegmentationDataset(Dataset):
-#     def __init__(self, image_dir, mask_dir, transform=None, patch_size=128, stride=128):
-#         self.image_dir = image_dir
-#         self.mask_dir = mask_dir
-#         self.transform = transform
-#         self.patch_size = patch_size
-#         self.stride = stride
-#         self.images = sorted(os.listdir(image_dir))
-#         self.masks = sorted(os.listdir(mask_dir))  # Ensure masks are sorted consistently
-#         self.image_to_mask = {img: f"mask_{img.split('_')[1]}" for img in self.images}
-
-#     def __len__(self):
-#         return len(self.images)
-
-#     def __getitem__(self, idx):
-#         img_name = self.images[idx]
-#         mask_name = self.image_to_mask.get(img_name, None)
-#         if mask_name is None:
-#             raise ValueError(f"No corresponding mask found for image: {img_name}")
-
-#         img_path = os.path.join(self.image_dir, img_name)
-#         mask_path = os.path.join(self.mask_dir, mask_name)
-
-#         image = cv2.imread(img_path)
-#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-
-#         # Generate patches for the image and mask
-#         patches = []
-#         for i in range(0, image.shape[0] - self.patch_size + 1, self.stride):
-#             for j in range(0, image.shape[1] - self.patch_size + 1, self.stride):
-#                 patch_img = image[i:i+self.patch_size, j:j+self.patch_size]
-#                 patch_mask = mask[i:i+self.patch_size, j:j+self.patch_size]
-
-#                 if self.transform:
-#                     augmented = self.transform(image=patch_img, mask=patch_mask)
-#                     patch_img = augmented['image']
-#                     patch_mask = augmented['mask']
-
-#                 patches.append((patch_img, patch_mask))
-
-#         return patches
-
-
 class SlumSegmentationDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transform=None, patch_size=128, stride=128):
         self.image_dir = image_dir
@@ -135,3 +58,37 @@ class SlumSegmentationDataset(Dataset):
             patch_mask = augmented['mask']
 
         return patch_img, patch_mask
+
+# class SlumSegmentationDataset(Dataset):
+#     def __init__(self, image_dir, mask_dir, transform=None):
+#         self.image_dir = image_dir
+#         self.mask_dir = mask_dir
+#         self.transform = transform
+#         self.images = sorted(os.listdir(image_dir))
+#         self.masks = sorted(os.listdir(mask_dir))  # Ensure masks are sorted consistently
+#         self.image_to_mask = {img: f"mask_{img.split('_')[1]}" for img in self.images}
+
+#     def __len__(self):
+#         return len(self.images)
+
+#     def __getitem__(self, idx):
+#         img_name = self.images[idx]
+#         mask_name = self.image_to_mask.get(img_name, None)
+#         if mask_name is None:
+#             raise ValueError(f"No corresponding mask found for image: {img_name}")
+
+#         img_path = os.path.join(self.image_dir, img_name)
+#         mask_path = os.path.join(self.mask_dir, mask_name)
+
+#         image = cv2.imread(img_path)
+#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+
+#         if self.transform:
+#             augmented = self.transform(image=image, mask=mask)
+#             image = augmented['image']
+#             mask = augmented['mask']
+
+#         return image, mask
+
+
